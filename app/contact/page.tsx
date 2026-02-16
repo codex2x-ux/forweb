cc77260ff5bcac1fffde3b822f67dbd681378f41
Deep <deep24mehra@gmail.com>
2026-02-17 05:20:35 +0530

    Add office location and map to contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -114,13 +114,21 @@ export default function ContactPage() {
     </div>
 
     {/* Map */}
+      <h3 className="text-xl font-semibold mb-4">
+  Visit Our Office
+</h3>
+<p className="text-neutral-400 mb-6">
+  New Delhi, India
+</p>
+      
     <div className="rounded-2xl overflow-hidden border border-neutral-800">
       <iframe
       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3501.438218451828!2d77.12865957466562!3d28.646594675657077!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d03ca7ced4493%3A0xbbac0a68f364ed82!2sVRM%20SERVICES!5e0!3m2!1sen!2sin!4v1771186376002!5m2!1sen!2sin"
       width="100%"
       height="350"
       style={{ border: 0 }}
-      allowFullScreen
+      allowFullScreen"
+      className="w-full h-[400px] grayscale hover:grayscale-0 transition duration-500
       loading="lazy"
         referrerPolicy="no-referrer-when-downgrade"
       ></iframe>
